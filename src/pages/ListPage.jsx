726e3260087a696e68dce21c94c4c4ca578e84ca
echo <play3step@naver.com>
2024-07-22 22:40:39 +0900

    Feat: sse 알림 세팅

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -7,6 +7,10 @@ import { useEffect } from 'react'
 import { setGroupList } from '../services/Group/groupController'
 import { getFavoriteList } from '../services/Favorite/favoriteController'
 
+import { EventSourcePolyfill } from 'event-source-polyfill'
+
+export const VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL
+
 const ListPage = () => {
   const userInfo = useSelector(state => state.user)
   const dispatch = useDispatch()
@@ -18,6 +22,45 @@ const ListPage = () => {
     dispatch(setGroupList(userInfo.token))
     dispatch(getFavoriteList(userInfo.token))
   }, [dispatch, userInfo.token])
+
+  useEffect(() => {
+    if (!userInfo.token) {
+      console.error('No user token found')
+      return
+    }
+
+    const sseUrl = `${VITE_SERVER_URL}/api/v1/notification/connect`
+
+    const sse = new EventSourcePolyfill(sseUrl, {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        Accept: 'text/event-stream'
+      },
+      withCredentials: true
+    })
+
+    // 연결 열림 이벤트
+    sse.onopen = () => {
+      console.log('Connection to server opened.')
+    }
+
+    // 메시지 수신 이벤트
+    sse.onmessage = e => {
+      console.log(JSON.parse(e.data))
+    }
+
+    // 오류 발생 이벤트
+    sse.onerror = e => {
+      console.error('EventSource error:', e)
+    }
+
+    // 컴포넌트 언마운트 시 SSE 연결 닫기
+    return () => {
+      if (sse) {
+        sse.close()
+      }
+    }
+  }, [userInfo.token])
 
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-[#F6F6F6]">
