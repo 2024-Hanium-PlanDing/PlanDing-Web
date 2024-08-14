75c24fcb18a4d3ab9c2c0300dc5cc1dea11bf8dc
echo <play3step@naver.com>
2024-08-14 15:49:29 +0900

    style: 오늘의 일정 스타일 overflow 수정

--- src/components/ListPage/Information/TodayList/TodayListContainer.jsx
+++ src/components/ListPage/Information/TodayList/TodayListContainer.jsx
@@ -3,15 +3,19 @@ import TodayList from './atom/TodayList'
 
 const TodayListContainer = ({ todaySchedule }) => {
   return (
-    <div className="w-[280px] h-[363px] border-primary-75 rounded-2xl border-2 p-5 flex flex-col gap-4">
+    <div className="w-[280px] h-[363px] border-primary-75 rounded-2xl border-2 p-4 flex flex-col gap-4">
       <p className="text-neutrals-800 text-subtitle-3">오늘의 일정</p>
-      {todaySchedule?.data.map((data, index) => (
-        <TodayList
-          data={data}
-          key={index}
-        />
-      ))}
-      <MoreBtn />
+      <div className="w-full h-full flex flex-col gap-4 overflow-y-auto overflow-x-hidden items-center">
+        {todaySchedule?.data.map((data, index) => (
+          <TodayList
+            data={data}
+            key={index}
+          />
+        ))}
+      </div>
+      <div>
+        <MoreBtn />
+      </div>
     </div>
   )
 }
