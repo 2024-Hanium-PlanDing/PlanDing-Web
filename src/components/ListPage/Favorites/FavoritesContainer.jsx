18b69757a673a304a29f639cefc87cb745172891
echo <play3step@naver.com>
2024-08-23 02:37:41 +0900

    style: 즐겨찾기 ui

--- src/components/ListPage/Favorites/FavoritesContainer.jsx
+++ src/components/ListPage/Favorites/FavoritesContainer.jsx
@@ -1,15 +1,29 @@
 import { useSelector } from 'react-redux'
 import FavoriteItem from './atom/FavoriteItem'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const FavoritesContainer = () => {
   const favoritesList = useSelector(state => state.favorite.favorites)
+  const [selectedItem, setSelectedItem] = useState(null)
+  const { code } = useParams() // URL에서 'code' 파라미터 추출
 
+  useEffect(() => {
+    if (code) {
+      setSelectedItem(code)
+    }
+  }, [code])
+  const handleSelectItem = code => {
+    setSelectedItem(code)
+  }
   return (
     <div className="w-[88px] h-[781px] py-10 px-6 mt-10 gap-10 shadow-md rounded-lg bg-white flex items-center flex-col">
       {favoritesList?.data?.map((data, index) => (
         <FavoriteItem
           data={data}
           key={index}
+          isSelected={data.code === selectedItem}
+          onSelectItem={handleSelectItem}
         />
       ))}
     </div>
--- src/components/ListPage/Favorites/atom/FavoriteItem.jsx
+++ src/components/ListPage/Favorites/atom/FavoriteItem.jsx
@@ -1,12 +1,17 @@
 import { useNavigate } from 'react-router-dom'
 
-const FavoriteItem = ({ data }) => {
+const FavoriteItem = ({ data, isSelected, onSelectItem }) => {
   const nav = useNavigate()
-
+  const handleClick = () => {
+    onSelectItem(data.code)
+    nav(`/group/${data.code}`)
+  }
   return (
     <div
-      className="w-[40px] h-[40px] rounded-full border border-black"
-      onClick={() => nav(`/group/${data.code}`)}>
+      className={`w-[40px] h-[40px] rounded-full border  
+      ${isSelected ? 'border-blue-500 border-2' : 'border-gray-300'} 
+      ${isSelected ? 'opacity-100' : 'opacity-50'}`}
+      onClick={handleClick}>
       <img
         className="w-full h-full"
         src={data.thumbnailPath}
