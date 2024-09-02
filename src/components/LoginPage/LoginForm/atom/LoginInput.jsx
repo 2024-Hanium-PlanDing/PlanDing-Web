05feccae2abc0baf869ef9e039bb8e0897ed451f
echo <play3step@naver.com>
2024-09-02 21:15:29 +0900

    test: s3 연결 확인

--- src/components/LoginPage/LoginForm/atom/LoginInput.jsx
+++ src/components/LoginPage/LoginForm/atom/LoginInput.jsx
@@ -5,6 +5,7 @@ const LoginInput = ({ text, textSize, textColor }) => {
       <input
         type="text"
         className="w-[360px] h-[48px] py-4 pl-5 border border-[#BEBEBE] rounded-md focus:outline-none focus:ring-0 focus:border-[#BEBEBE]"
+        disabled
       />
     </div>
   )
